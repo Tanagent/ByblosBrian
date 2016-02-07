06492d51d49832de7a7e4b2a0b5cfb057f2e5b1b
Brian Tan <Brian Tan>
2016-02-06 16:58:33 -0800

    added some UI stuff

---
+++ Byblos/app/src/main/java/com/example/brian/byblos/AccountSetup.java
@@ -0,0 +1,18 @@
+package com.example.brian.byblos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by Brian on 2/6/2016.
+ */
+public class AccountSetup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.account_setup);
+    }
+}
